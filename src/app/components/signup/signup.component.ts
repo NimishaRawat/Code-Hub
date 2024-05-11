ef1caef5265da2a09f0b6695b63550cc0de8f167
Nimisha Rawat <nimisha17rawat@gmail.com>
2024-05-11 21:59:03 +0530

    Angular 17 project
    Code snippets are stored here.

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [FormsModule,NgIf],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+  constructor(private authService:AuthService ,private router:Router){}
+
+  register(regForm:NgForm){
+    console.log(regForm.value)
+    this.authService.registerUser(regForm.value.email,regForm.value.password)
+
+  }
+  reset(regForm:NgForm){
+    regForm.reset()
+  }
+}
