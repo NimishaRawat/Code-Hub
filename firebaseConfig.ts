088e94fcee26caeff5485c6c5c047d7633f6f1cf
Nimisha Rawat <nimisha17rawat@gmail.com>
2024-05-11 21:43:06 +0530

    Initial commit

---
+++ firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBdUt67Yb_ORIcV5D-fZSx6r-720j-Hh2M",
+    authDomain: "code-bin-angular.firebaseapp.com",
+    projectId: "code-bin-angular",
+    storageBucket: "code-bin-angular.appspot.com",
+    messagingSenderId: "422965864686",
+    appId: "1:422965864686:web:6234d9fb5cb0e232552bff"
+  };
